9e7d6a8e5d2b0c7a490919a3bcd0d882a79f3897
Tom M <tom.mbrt@googlemail.com>
2016-02-19 22:04:25 +0100

    add common.cpp

---
+++ common.cpp
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+// helper function for case insensitive string compare
+// will workon all platforms, but probably slow
+bool iEqualsUgly(string strFirst, string strSecond)
+{
+  // convert strings to upper case before compare
+  transform(strFirst.begin(), strFirst.end(), strFirst.begin(), toupper);
+  transform(strSecond.begin(), strSecond.end(), strSecond.begin(), toupper);
+  return strFirst == strSecond;
+}
+
+// helper function for case insensitive string compare
+bool iEquals(const string& str1, const string& str2)
+{
+#ifdef POSIX
+  return strncasecmp(str1.c_str(), str2.c_str(), min(str1.size(),str2.size())) == 0;  
+#else
+  return iEqualsUgly(str1, str2);
+#endif
+}
+
+string getFileExtension(const string& filePath)
+{
+    return filePath.substr(filePath.find_last_of(".") + 1);
+}
