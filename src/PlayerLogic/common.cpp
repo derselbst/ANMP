79ec2b676cea5b0b69f21da52e195d2793debc33
derselbst <tom.mbrt@googlemail.com>
2016-02-21 14:33:15 +0100

    organize folder structure

--- src/PlayerLogic/common.cpp
+++ src/PlayerLogic/common.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+#ifndef _POSIX_VERSION
 // helper function for case insensitive string compare
 // will workon all platforms, but probably slow
 bool iEqualsUgly(string strFirst, string strSecond)
@@ -14,11 +15,12 @@ bool iEqualsUgly(string strFirst, string strSecond)
   transform(strSecond.begin(), strSecond.end(), strSecond.begin(), toupper);
   return strFirst == strSecond;
 }
+#endif
 
 // helper function for case insensitive string compare
 bool iEquals(const string& str1, const string& str2)
 {
-#ifdef POSIX
+#ifdef _POSIX_VERSION
   return strncasecmp(str1.c_str(), str2.c_str(), min(str1.size(),str2.size())) == 0;  
 #else
   return iEqualsUgly(str1, str2);
