c798b23ec0e6e2f9f50561cc262e032e5f99786c
derselbst <tom.mbrt@googlemail.com>
2016-02-27 17:28:47 +0100

    added NotInitializedException

--- src/Common/CommonExceptions.h
+++ src/Common/CommonExceptions.h
@@ -7,3 +7,9 @@ class NotImplementedException : public std::logic_error
 public:
   NotImplementedException():std::logic_error("Case or Function not yet implemented"){}
 }; 
+
+class NotInitializedException : public std::runtime_error
+{
+public:
+  NotInitializedException():std::runtime_error("Object not initialized, call init() first!"){}
+}; 
