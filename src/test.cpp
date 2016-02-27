8be97ca7f784af12d87f3edbe1234121c52512eb
derselbst <tom.mbrt@googlemail.com>
2016-02-27 17:33:34 +0100

    update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "unistd.h"
 
 #include "Playlist.h"
 #include "PlaylistFactory.h"
@@ -9,21 +10,35 @@ using namespace std;
 
 int main()
 {
- string filenames = "/home/tom/Eigene Programme/ANMP/src/bmenuhard.wav";
+ vector<string> filenames;
+filenames.push_back("somewav.wav");
+filenames.push_back("someother.wav");
  
  
  IPlaylist* plist = new Playlist();
  
- PlaylistFactory::addSong(*plist, filenames);
+ PlaylistFactory::addSongs(*plist, filenames);
  
  Player p(plist);
 
    
    p.init();
    p.play();
-   while(true);
-  
- 
+      sleep(2);
+   cout << "seek" <<endl;
+   p.seekTo(200000);
+
+   sleep(10);
+cout << "stop" <<endl;
+   p.stop();
+   cout << "play" <<endl;
+  p.play();
+  cout << "achtung" <<endl;
+    sleep(5);
+    cout << "pause" <<endl;
+   p.pause();
+   p.play();
+   sleep(5);
  return 0;
  
  
