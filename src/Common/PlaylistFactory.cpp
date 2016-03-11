df67da05f7c6570cce98fe9ec7c4acb905ab427a
derselbst <tom.mbrt@googlemail.com>
2016-03-11 16:26:35 +0100

    started implementing cue sheet support

--- src/Common/PlaylistFactory.cpp
+++ src/Common/PlaylistFactory.cpp
@@ -18,6 +18,46 @@
  * @param  filePath
  * @param  offset
  */
+
+PlaylistFactory::parseCue(string& filepath)
+{
+#define cue_assert(ERRMSG, COND) \
+  if(!(COND))\
+  {\
+     throw runtime_error(string("Error: Failed to parse CUE-Sheet file \"") + filepath + "\" ("+ERRMSG+")");\
+  }
+  
+  FILE *f = fopen(filepath.c_str());
+  Cd *cd = cue_parse_file(f);
+  cue_assert ("error parsing CUE", cd != NULL);
+  
+  int ntrk = cd_get_ntrack (cd);
+   for(int i=0; i< ntrk; i++)
+   {
+      Track* track = cd_get_track (cd, 1);
+      cue_assert ("error getting track", track != NULL);
+
+      val = track_get_filename (track);
+      cue_assert ("error getting track filename", val != NULL);
+
+      cdtext = track_get_cdtext (track);
+      cue_assert ("error getting track CDTEXT", cdtext != NULL);
+
+      val = cdtext_get (PTI_PERFORMER, cdtext);
+      cue_assert ("error getting track performer", val != NULL);
+
+      val = cdtext_get (PTI_TITLE, cdtext);
+      cue_assert ("error getting track title", val != NULL);
+
+      int strangeFramesStart = track_get_start (track);
+      strangeFramesStart*1000/75
+      
+      int strangeFramesLen = track_get_length (track);
+   }
+  cd_delete (cd);
+#undefine cue_assert
+}
+
 Song* PlaylistFactory::addSong (IPlaylist& playlist, string filePath, string offset)
 {
     string ext = getFileExtension(filePath);
