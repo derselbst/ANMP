cb115d632e1a1926df97b72f52e0181769b820b7
derselbst <tom.mbrt@googlemail.com>
2016-03-12 18:27:29 +0100

    started implementing a custom playlist model
    addresses #2

---
+++ src/GUI/PlaylistModel.h
@@ -0,0 +1,54 @@
+ 
+class PlaylistModel : public QAbstractTableModel, public Playlist
+{
+public:
+    PlaylistModel(QObject *parent = 0);
+    
+    // *** MODEL READ SUPPORT ***
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    
+    QVariant data(const QModelIndex &index, int role) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    
+    
+    // *** MODEL WRITE SUPPORT ***
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    
+    
+    
+    
+    
+    /**
+     * @param  song
+     */
+    void add (Song* song) override;
+
+    /**
+     */
+    void remove (Song* song) override;
+
+    /**
+     */
+    void remove (int i) override;
+
+    void clear() override;
+
+    /**
+     */
+    Song* next () override;
+
+    /**
+     */
+    Song* previous () override;
+
+    Song* getSong(unsigned int id) override;
+
+    /**
+     */
+    Song* current () override;
+    
+private:
+
+};
