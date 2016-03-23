5d45448f2dbdbbf22ad1e3729d01712571c9dec0
derselbst <tom.mbrt@googlemail.com>
2016-03-23 18:49:27 +0100

    add some fade vars

--- src/PlayerLogic/Config.cpp
+++ src/PlayerLogic/Config.cpp
@@ -4,11 +4,20 @@
 //
 
 //**********************************
-//         GLOBAL SECTION          *
+//     GLOBAL INTERNAL SECTION     *
 //**********************************
 
 // indicates the default audio driver to use
 AudioDriver_t Config::audioDriver = AudioDriver_t::ALSA;
+
+// number of frames that are pushed to audioDriver during each run AND minimum no. of frames
+// that have to be prepared (i.e. rendered) by a single call to Song::fillBuffer()
+const frame_t Config::FramesToRender = 2048;
+
+
+//**********************************
+//       HOW-TO-PLAY SECTION       *
+//**********************************
 
 // whether the loop information a track may contain, shall be used
 bool Config::useLoopInfo = true;
@@ -16,11 +25,14 @@ bool Config::useLoopInfo = true;
 // play each and every loop that many time, -1 will ignore this setting
 int Config::overridingGlobalLoopCount = -1;
 
-// number of frames that are pushed to audioDriver during each run AND minimum no. of frames
-// that have to be prepared (i.e. rendered) by a single call to Song::fillBuffer()
-const frame_t Config::FramesToRender = 2048;
+// time in milliseconds to fade out a song (before it ends), if supported by the underlying WrapperLib
+unsigned int defaultFadeTime = 3500;
 
+// time in milliseconds to fade out a song when the user hits stop
+unsigned int fadeTimeStop = 3500;
 
+// time in milliseconds to fade out a song when the user hits pause
+unsigned int fadeTimePause = 3500;
 
 //**********************************
 //      USF-SPECIFIC SECTION       *
