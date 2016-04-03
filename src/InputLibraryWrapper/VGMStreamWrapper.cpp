a15444f232c369482d5c77bf53cc1c12f5c8b8a5
derselbst <tom.mbrt@googlemail.com>
2016-04-03 20:55:51 +0200

    format

--- src/InputLibraryWrapper/VGMStreamWrapper.cpp
+++ src/InputLibraryWrapper/VGMStreamWrapper.cpp
@@ -6,28 +6,28 @@
 
 extern "C"
 {
-  #include <util.h>
+#include <util.h>
 }
 // Constructors/Destructors
 //
 
 VGMStreamWrapper::VGMStreamWrapper(string filename, size_t fileOffset, size_t fileLen) : Song(filename, fileOffset, fileLen)
 {
-  this->Format.SampleFormat = SampleFormat_t::int16;
+    this->Format.SampleFormat = SampleFormat_t::int16;
 }
 
 VGMStreamWrapper::~VGMStreamWrapper ()
 {
-  this->releaseBuffer();
-  this->close();
+    this->releaseBuffer();
+    this->close();
 }
 
 void VGMStreamWrapper::open()
 {
-  if(this->handle!=nullptr)
-  {
-    return;
-  }
+    if(this->handle!=nullptr)
+    {
+        return;
+    }
 
     this->handle = init_vgmstream(this->Filename.c_str());
 
@@ -35,7 +35,7 @@ void VGMStreamWrapper::open()
     {
         throw runtime_error(string("Error: ") + __func__ + string(": failed opening \"") + this->Filename + "\"");
     }
-    
+
     this->Format.Channels = this->handle->channels;
     this->Format.SampleRate = this->handle->sample_rate;
 }
@@ -43,10 +43,10 @@ void VGMStreamWrapper::open()
 void VGMStreamWrapper::close()
 {
     if(this->handle!=nullptr)
-  {
-    close_vgmstream (this->handle);
-    this->handle=nullptr;
-  }
+    {
+        close_vgmstream (this->handle);
+        this->handle=nullptr;
+    }
 }
 
 void VGMStreamWrapper::fillBuffer()
@@ -55,14 +55,14 @@ void VGMStreamWrapper::fillBuffer()
     {
         this->count = this->getFrames() * this->Format.Channels;
         this->data = new int16_t[this->count];
-	CLOG(AtomicWrite::LogLevel::DEBUG, "vgmstream allocated buffer at 0x" << this->data << endl);
+        CLOG(AtomicWrite::LogLevel::DEBUG, "vgmstream allocated buffer at 0x" << this->data << endl);
 
         // usually this shouldnt block at all
         WAIT(this->futureFillBuffer);
 
-	// (pre-)render the first 1/3 second
-	this->render(msToFrames(333, this->Format.SampleRate));
-	
+        // (pre-)render the first 1/3 second
+        this->render(msToFrames(333, this->Format.SampleRate));
+
         // immediatly start filling the pcm buffer
         this->futureFillBuffer = async(launch::async, &VGMStreamWrapper::render, this, 0);
 
@@ -73,30 +73,30 @@ void VGMStreamWrapper::fillBuffer()
 
 void VGMStreamWrapper::render(frame_t framesToRender)
 {
-  if(framesToRender==0)
-  {
-    // render rest of file
-    framesToRender = this->getFrames()-this->framesAlreadyRendered;
-  }
-  else
-  {
-    framesToRender = min(framesToRender, this->getFrames()-this->framesAlreadyRendered);
-  }
-  
-  int16_t* pcm = static_cast<int16_t*>(this->data);
-  pcm += this->framesAlreadyRendered * this->Format.Channels;
-  
-  int framesToDoNow;
-  while(framesToRender>0 && !this->stopFillBuffer)
-  {
+    if(framesToRender==0)
+    {
+        // render rest of file
+        framesToRender = this->getFrames()-this->framesAlreadyRendered;
+    }
+    else
+    {
+        framesToRender = min(framesToRender, this->getFrames()-this->framesAlreadyRendered);
+    }
+
+    int16_t* pcm = static_cast<int16_t*>(this->data);
+    pcm += this->framesAlreadyRendered * this->Format.Channels;
+
+    int framesToDoNow;
+    while(framesToRender>0 && !this->stopFillBuffer)
+    {
         framesToDoNow = (framesToRender/Config::FramesToRender)>0 ? Config::FramesToRender : framesToRender%Config::FramesToRender;
-    
+
         render_vgmstream(pcm, framesToDoNow, this->handle);
         pcm += framesToDoNow * this->Format.Channels;
         this->framesAlreadyRendered += framesToDoNow;
 
         framesToRender -= framesToDoNow;
-  }
+    }
 }
 
 void VGMStreamWrapper::releaseBuffer()
@@ -116,7 +116,7 @@ vector<loop_t> VGMStreamWrapper::getLoopArray () const
 {
     vector<loop_t> res;
 
-        // TODO implement
+    // TODO implement
 //     /* force only if there aren't already loop points */
 //     if (force_loop && !handle->loop_flag) {
 //         /* this requires a bit more messing with the VGMSTREAM than I'm
@@ -126,7 +126,7 @@ vector<loop_t> VGMStreamWrapper::getLoopArray () const
 //         handle->loop_end_sample=handle->num_samples;
 //         handle->loop_ch=calloc(handle->channels,sizeof(VGMSTREAMCHANNEL));
 //     }
-// 
+//
 //     /* force even if there are loop points */
 //     if (really_force_loop) {
 //         if (!handle->loop_flag) handle->loop_ch=calloc(handle->channels,sizeof(VGMSTREAMCHANNEL));
@@ -134,27 +134,27 @@ vector<loop_t> VGMStreamWrapper::getLoopArray () const
 //         handle->loop_start_sample=0;
 //         handle->loop_end_sample=handle->num_samples;
 //     }
-// 
+//
 //     if (ignore_loop) handle->loop_flag=0;
-    
-if(this->handle!=nullptr && this->handle->loop_flag==1) // does stream contain loop information?
-{
-    loop_t l;
-    l.start = handle->loop_start_sample;
-    l.stop = handle->loop_end_sample;
-    //TODO let the user adjust loopcount
-    l.count = 2;
-    res.push_back(l);
-}
+
+    if(this->handle!=nullptr && this->handle->loop_flag==1) // does stream contain loop information?
+    {
+        loop_t l;
+        l.start = handle->loop_start_sample;
+        l.stop = handle->loop_end_sample;
+        //TODO let the user adjust loopcount
+        l.count = 2;
+        res.push_back(l);
+    }
 
     return res;
 }
 
 frame_t VGMStreamWrapper::getFrames() const
 {
-  if(this->handle==nullptr)
-  {
-    return 0;
-  }
-  return this->handle->num_samples;
+    if(this->handle==nullptr)
+    {
+        return 0;
+    }
+    return this->handle->num_samples;
 }
