96ecb7892e457affde02649512767a6e0b31ae8a
derselbst <tom.mbrt@googlemail.com>
2016-04-05 19:49:04 +0200

    add config dialog for GUI

---
+++ src/GUI/configdialog.cpp
@@ -0,0 +1,14 @@
+#include "configdialog.h"
+#include "ui_configdialog.h"
+
+ConfigDialog::ConfigDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ConfigDialog)
+{
+    ui->setupUi(this);
+}
+
+ConfigDialog::~ConfigDialog()
+{
+    delete ui;
+}
