c6b9f7df1a113097ca99b569c50d1f6e6bc51e6c
derselbst <tom.mbrt@googlemail.com>
2016-04-06 15:41:29 +0200

    fix PlaylistFactory

--- src/Common/PlaylistFactory.cpp
+++ src/Common/PlaylistFactory.cpp
@@ -191,16 +191,16 @@ Song* PlaylistFactory::addSong (IPlaylist& playlist, const string filePath, size
       TRY_WITH(VGMStreamWrapper)
 #endif
 
+#ifdef USE_LIBGME
+      TRY_WITH(LibGMEWrapper)
+#endif
+
 // !!! libmad always has to be last !!!
 // libmad eats up every garbage of binary (= non MPEG audio shit)
 // thus always make sure libmad is the very last try to read any audio file
 #ifdef USE_LIBMAD
       l_LIBMAD:
       TRY_WITH(LibMadWrapper)
-#endif
-      
-#ifdef USE_LIBGME
-      TRY_WITH(LibGMEWrapper)
 #endif
     }
 
