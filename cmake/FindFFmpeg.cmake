36130268614a743a946fba8a1061cc0e838e1782
derselbst <tom.mbrt@gmail.com>
2016-04-24 20:51:32 +0200

    fix build

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -1,78 +1,143 @@
-# - Try to find ffmpeg libraries (libavcodec, libavformat and libavutil)
-# Once done this will define
+# FindFFMPEG
+# ----------
 #
-# FFMPEG_FOUND - system has ffmpeg or libav
-# FFMPEG_INCLUDE_DIR - the ffmpeg include directory
-# FFMPEG_LIBRARIES - Link these to use ffmpeg
-# FFMPEG_LIBAVCODEC
-# FFMPEG_LIBAVFORMAT
-# FFMPEG_LIBAVUTIL
+# Find the native FFMPEG includes and libraries
 #
-# Copyright (c) 2008 Andreas Schneider <mail@cynapses.org>
-# Modified for other libraries by Lasse Kärkkäinen <tronic>
-# Modified for Hedgewars by Stepik777
+# This module defines:
 #
-# Redistribution and use is allowed according to the terms of the New
-# BSD license.
+#  FFMPEG_INCLUDE_DIR, where to find avformat.h, avcodec.h...
+#  FFMPEG_LIBRARIES, the libraries to link against to use FFMPEG.
+#  FFMPEG_FOUND, If false, do not try to use FFMPEG.
 #
+# also defined, but not for general use are:
+#
+#   FFMPEG_avformat_LIBRARY, where to find the FFMPEG avformat library.
+#   FFMPEG_avcodec_LIBRARY, where to find the FFMPEG avcodec library.
+#
+# This is useful to do it this way so that we can always add more libraries
+# if needed to ``FFMPEG_LIBRARIES`` if ffmpeg ever changes...
 
-if (FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
-# in cache already
-set(FFMPEG_FOUND TRUE)
-else (FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
-# use pkg-config to get the directories and then use these values
-# in the FIND_PATH() and FIND_LIBRARY() calls
-find_package(PkgConfig)
-if (PKG_CONFIG_FOUND)
-pkg_check_modules(_FFMPEG_AVCODEC libavcodec)
-pkg_check_modules(_FFMPEG_AVFORMAT libavformat)
-pkg_check_modules(_FFMPEG_AVUTIL libavutil)
-endif (PKG_CONFIG_FOUND)
+#=============================================================================
+# Copyright: 1993-2008 Ken Martin, Will Schroeder, Bill Lorensen
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of ppsspp, substitute the full
+#  License text for the above reference.)
 
-find_path(FFMPEG_AVCODEC_INCLUDE_DIR
-NAMES libavcodec/avcodec.h
-PATHS ${_FFMPEG_AVCODEC_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include /sw/include
-PATH_SUFFIXES ffmpeg libav
+find_path(FFMPEG_INCLUDE_DIR1 libavformat/avformat.h
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+#   $ENV{FFMPEG_DIR}/libavformat
+#   $ENV{FFMPEG_DIR}/include/libavformat
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+#   /usr/include/libavformat
+#   /usr/include/ffmpeg/libavformat
+#   /usr/local/include/libavformat
 )
 
-find_library(FFMPEG_LIBAVCODEC
-NAMES avcodec
-PATHS ${_FFMPEG_AVCODEC_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+find_path(FFMPEG_INCLUDE_DIR2 libavcodec/avcodec.h
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+#   $ENV{FFMPEG_DIR}/libavcodec
+#   $ENV{FFMPEG_DIR}/include/libavcodec
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+#   /usr/include/libavcodec
+#   /usr/include/ffmpeg/libavcodec
+#   /usr/local/include/libavcodec
 )
 
-find_library(FFMPEG_LIBAVFORMAT
-NAMES avformat
-PATHS ${_FFMPEG_AVFORMAT_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+find_path(FFMPEG_INCLUDE_DIR3 libavutil/avutil.h
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+#   $ENV{FFMPEG_DIR}/libavutil
+#   $ENV{FFMPEG_DIR}/include/libavutil
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+#   /usr/include/libavutil
+#   /usr/include/ffmpeg/libavutil
+#   /usr/local/include/libavutil
 )
 
-find_library(FFMPEG_LIBAVUTIL
-NAMES avutil
-PATHS ${_FFMPEG_AVUTIL_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+find_path(FFMPEG_INCLUDE_DIR4 libswresample/swresample.h
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/ffmpeg
+#   $ENV{FFMPEG_DIR}/libswresample
+#   $ENV{FFMPEG_DIR}/include/libswresample
+  $ENV{FFMPEG_DIR}/include/ffmpeg
+  /usr/local/include/ffmpeg
+  /usr/include/ffmpeg
+#   /usr/include/libswresample
+#   /usr/include/ffmpeg/libswresample
+#   /usr/local/include/libswresample
 )
 
-if (FFMPEG_LIBAVCODEC AND FFMPEG_LIBAVFORMAT)
-set(FFMPEG_FOUND TRUE)
+if(FFMPEG_INCLUDE_DIR1 AND
+   FFMPEG_INCLUDE_DIR2 AND
+   FFMPEG_INCLUDE_DIR3 AND
+   FFMPEG_INCLUDE_DIR4
+)
+  set(FFMPEG_INCLUDE_DIR ${FFMPEG_INCLUDE_DIR1}
+                         ${FFMPEG_INCLUDE_DIR2}
+                         ${FFMPEG_INCLUDE_DIR3}
+                         ${FFMPEG_INCLUDE_DIR4}
+  )
 endif()
 
-if (FFMPEG_FOUND)
-set(FFMPEG_INCLUDE_DIR ${FFMPEG_AVCODEC_INCLUDE_DIR})
-
-set(FFMPEG_LIBRARIES
-${FFMPEG_LIBAVCODEC}
-${FFMPEG_LIBAVFORMAT}
-${FFMPEG_LIBAVUTIL}
+find_library(FFMPEG_avformat_LIBRARY avformat
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/lib${CMAKE_LIBDIR_SUFFIX}
+  $ENV{FFMPEG_DIR}/libavformat
+  /usr/local/lib${CMAKE_LIBDIR_SUFFIX}
+  /usr/lib${CMAKE_LIBDIR_SUFFIX}
 )
 
-endif (FFMPEG_FOUND)
+find_library(FFMPEG_avcodec_LIBRARY avcodec
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/lib${CMAKE_LIBDIR_SUFFIX}
+  $ENV{FFMPEG_DIR}/libavcodec
+  /usr/local/lib${CMAKE_LIBDIR_SUFFIX}
+  /usr/lib${CMAKE_LIBDIR_SUFFIX}
+)
 
-if (FFMPEG_FOUND)
-if (NOT FFMPEG_FIND_QUIETLY)
-message(STATUS "Found FFMPEG or Libav: ${FFMPEG_LIBRARIES}, ${FFMPEG_INCLUDE_DIR}")
-endif (NOT FFMPEG_FIND_QUIETLY)
-else (FFMPEG_FOUND)
-if (FFMPEG_FIND_REQUIRED)
-message(FATAL_ERROR "Could not find libavcodec or libavformat or libavutil")
-endif (FFMPEG_FIND_REQUIRED)
-endif (FFMPEG_FOUND)
+find_library(FFMPEG_avutil_LIBRARY avutil
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/lib${CMAKE_LIBDIR_SUFFIX}
+  $ENV{FFMPEG_DIR}/libavutil
+  /usr/local/lib${CMAKE_LIBDIR_SUFFIX}
+  /usr/lib${CMAKE_LIBDIR_SUFFIX}
+)
 
-endif (FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
+find_library(FFMPEG_swresample_LIBRARY swresample
+  $ENV{FFMPEG_DIR}
+  $ENV{FFMPEG_DIR}/lib${CMAKE_LIBDIR_SUFFIX}
+  $ENV{FFMPEG_DIR}/libswresample
+  /usr/local/lib${CMAKE_LIBDIR_SUFFIX}
+  /usr/lib${CMAKE_LIBDIR_SUFFIX}
+)
+
+if(FFMPEG_INCLUDE_DIR)
+  if(FFMPEG_avformat_LIBRARY AND
+     FFMPEG_avcodec_LIBRARY AND
+     FFMPEG_avutil_LIBRARY AND
+     FFMPEG_swresample_LIBRARY
+  )
+    set(FFMPEG_FOUND "TRUE")
+    set(FFMPEG_LIBRARIES ${FFMPEG_avformat_LIBRARY}
+                         ${FFMPEG_avcodec_LIBRARY}
+                         ${FFMPEG_avutil_LIBRARY}
+                         ${FFMPEG_swresample_LIBRARY}
+    )
+  endif()
+endif()
+
--- src/InputLibraryWrapper/FFMpegWrapper.h
+++ src/InputLibraryWrapper/FFMpegWrapper.h
@@ -7,8 +7,8 @@ extern "C"
 {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
-  #include "libswresample/swresample.h"
-  #include <libavutil/opt.h>
+#include <libswresample/swresample.h>
+#include <libavutil/opt.h>
 }
 
 /**
