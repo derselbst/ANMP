500bab4ed3b1069b1d70ba7799e91fdd45a3cea9
Tom M <tom.mbrt@googlemail.com>
2016-07-08 07:16:14 +0200

    add first untested portaudio wrapper

---
+++ src/AudioOutput/PortAudioOutput.cpp
@@ -0,0 +1,179 @@
+#include "PortAudioOutput.h"
+
+#include "CommonExceptions.h"
+#include "AtomicWrite.h"
+
+#include <iostream>
+#include <string>
+
+
+PortAudioOutput::PortAudioOutput ()
+{
+}
+
+PortAudioOutput::~PortAudioOutput ()
+{
+    this->close();
+	
+	if (this->paInitError == PaErrorCode::paNoError)
+	{
+		Pa_Terminate();
+	}
+}
+
+//
+// Interface Implementation
+//
+void PortAudioOutput::open()
+{
+	if(this->handle != nullptr)
+	{
+		this->paInitError = Pa_Initialize();
+		if (this->paInitError != PaErrorCode::paNoError)
+		{
+			THROW_RUNTIME_ERROR("unable to initialize portaudio (" << Pa_GetErrorText(this->paInitError) << ")");
+		}
+	}
+}
+
+void PortAudioOutput::init(unsigned int sampleRate, uint8_t channels, SampleFormat_t s, bool realtime)
+{
+	if (this->paInitError != PaErrorCode::paNoError)
+	{
+		THROW_RUNTIME_ERROR("portaudio not initialized! (" << Pa_GetErrorText(this->paInitError) << ")");
+	}
+	
+	PaSampleFormat paSampleFmt;
+	
+    switch(s)
+    {
+    case float32:
+        paSampleFmt = paFloat32;
+        break;
+    case int16:
+        paSampleFmt =  paInt16;
+        break;
+    case int32:
+        paSampleFmt =  paInt32;
+        break;
+    case unknown:
+        THROW_RUNTIME_ERROR("Sample Format not set");
+
+    default:
+        throw NotImplementedException();
+        break;
+    }
+	
+	this->drop();
+	this->close();
+
+    PaError err;
+
+	/* Open an audio I/O stream. */
+    err = Pa_OpenDefaultStream( &this->handle,
+                                0,          /* no input channels */
+                                channels,   /* no. of output channels */
+                                paSampleFmt,  /* 32 bit floating point output */
+                                sampleRate,
+                                Config::FramesToRender,  /* frames per buffer, i.e. the number of sample frames that PortAudio will request from the callback.*/
+                                nullptr, /* this is my callback function */
+                                this ); /* This is a pointer that will be passed to my callback */
+	
+	
+	
+
+    // finally update channelcount, srate and sformat
+    this->currentChannelCount = channels;
+    this->currentSampleFormat = s;
+    this->currentSampleRate   = sampleRate;
+}
+
+void PortAudioOutput::drain()
+{
+    this->stop();
+}
+
+void PortAudioOutput::drop()
+{
+    this->stop();
+}
+
+void PortAudioOutput::close()
+{
+    if (this->handle != nullptr)
+    {
+        Pa_CloseStream( this->handle );
+        this->handle = nullptr;
+    }
+}
+
+int PortAudioOutput::write (float* buffer, frame_t frames)
+{
+    return this->write<float>(buffer, frames);
+}
+
+int PortAudioOutput::write (int16_t* buffer, frame_t frames)
+{
+    return this->write<int16_t>(buffer, frames);
+}
+
+int PortAudioOutput::write (int32_t* buffer, frame_t frames)
+{
+    return this->write<int32_t>(buffer, frames);
+}
+
+template<typename T> int PortAudioOutput::write(T* buffer, frame_t frames)
+{   
+ 	if(this->handle == nullptr)
+	{
+		THROW_RUNTIME_ERROR("unable to write pcm since PortAudioOutput::init() has not been called yet or init failed");
+	}
+	
+    const int items = frames*this->currentChannelCount;
+    T processedBuffer[items];
+    
+    memcpy(processedBuffer, buffer, items*sizeof(T));
+    
+    for(int i = 0; i<items; i++)
+    {
+        processedBuffer[i] = buffer[i] * this->volume;
+    }
+    
+    buffer = processedBuffer;
+    
+    Pa_WriteStream( this->handle, buffer, frames );
+}
+
+void PortAudioOutput::setVolume(float vol)
+{
+    this->volume=vol;
+}
+
+void PortAudioOutput::start()
+{
+	if(this->handle != nullptr)
+	{
+		PaError err = Pa_StartStream( this->handle );
+		if (err != PaErrorCode::paNoError)
+		{
+			THROW_RUNTIME_ERROR("unable to start pcm (" << Pa_GetErrorText(err) << ")");
+		}
+	}
+	else
+	{
+		THROW_RUNTIME_ERROR("unable to start pcm since PortAudioOutput::init() has not been called yet or init failed");
+	}
+}
+
+void PortAudioOutput::stop()
+{
+	if(this->handle != nullptr)
+	{
+		PaError err = Pa_StopStream( this->handle );
+		if (err != PaErrorCode::paNoError)
+		{
+			THROW_RUNTIME_ERROR("unable to stop pcm (" << Pa_GetErrorText(err) << ")");
+		}
+	}
+}
+
---
+++ src/AudioOutput/PortAudioOutput.h
@@ -0,0 +1,68 @@
+#ifndef PortAudioOutput_H
+#define PortAudioOutput_H
+
+#include "IAudioOutput.h"
+
+#include <portaudio.h>
+
+
+/**
+  * class PortAudioOutput
+  *
+  * A wrapper library for portaudio, enabling ANMP for crossplatform playback
+  */
+class PortAudioOutput : public IAudioOutput
+{
+public:
+
+    PortAudioOutput ();
+
+    // forbid copying
+    PortAudioOutput(PortAudioOutput const&) = delete;
+    PortAudioOutput& operator=(PortAudioOutput const&) = delete;
+    
+    virtual ~PortAudioOutput ();
+
+
+    // interface methods declaration
+
+    void open () override;
+
+    void init (unsigned int sampleRate, uint8_t channels, SampleFormat_t s, bool realtime = false) override;
+
+    void close () override;
+
+    int write (float* buffer, frame_t frames) override;
+
+    int write (int16_t* buffer, frame_t frames) override;
+
+    int write (int32_t* buffer, frame_t frames) override;
+
+    void setVolume(float) override;
+
+    void start () override;
+    void stop () override;
+
+private:
+
+	PaStream *handle = nullptr;
+	PaError paInitError = ~PaErrorCode::paNoError
+    
+    // the current volume [0,1.0] to use
+    // mark this as volatile so the compiler doesnt come up with:
+    // "oh, this member isnt modified in the current scope. lets put it to a register, while using this var inside a loop again and again."
+    // volatile here hopefully forces that each read actually happens through memory (I dont worry too much about hardware caching here)
+    // NOTE: I dont need thread safety for this variable and I know that volatile doesnt do anything for that either. its just because
+    // one thread will always read from this var (which happens in this->write(T*, frame_t)) and another thread occasionally comes along and alters
+    // this var (inside this->setVolume())
+    // the worst things that can happen here are dirty reads, as far as I see; and who cares?
+    // however, Im not absolutely sure if volatile if really required here
+    volatile float volume = 1.0f;
+
+    template<typename T> int write(T* buffer, frame_t frames);
+    void drain ();
+    void drop ();
+
+};
+
+#endif // PortAudioOutput_H
