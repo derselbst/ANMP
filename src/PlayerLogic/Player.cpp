8bd6e3619c0209d52fb27627cfddd87df8cb0536
derselbst <tom.mbrt@googlemail.com>
2016-07-09 16:21:52 +0200

    always make sure pcm stream is started

--- src/PlayerLogic/Player.cpp
+++ src/PlayerLogic/Player.cpp
@@ -533,9 +533,9 @@ void Player::playInternal ()
 {
     try
     {
-        this->audioDriver->start();
         while(this->isPlaying)
         {
+        this->audioDriver->start();
             core::tree<loop_t>& loops = this->currentSong->loopTree;
 
             this->playLoop(loops);
