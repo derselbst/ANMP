2ee1ebf47620e306ca03b8dfc7ef5e67b2c96bb8
derselbst <tom.mbrt@gmail.com>
2016-12-31 17:33:05 +0100

    update TestCommon

--- test/TestCommon.cpp
+++ test/TestCommon.cpp
@@ -9,8 +9,29 @@ using namespace std;
 
 int main()
 {
-
+    bool equal = iEquals("qwer", "qwer");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("poiuasdfyxcvqwer", "poiuasdfyxcvqwer");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("1234", "1234");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("asdfasdf ASDFaSdF", "aSDFAsdf aSdFasDf");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("XYZ123", "xyz123");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("", "");
+    TEST_ASSERT(equal == true);
+    
+    equal = iEquals("aaaa", "aaa");
+    TEST_ASSERT(equal == false);
+    
+    equal = iEquals("zzz", "ZZZz");
+    TEST_ASSERT(equal == false);
+    
     return 0;
-
-
 }
