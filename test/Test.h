ca0a3abcb83ce4c174590a6ac7cf84adf1ee4eb2
Tom M <tom.mbrt@googlemail.com>
2017-01-05 09:23:27 +0100

    add TEST_ASSERT_EQ

--- test/Test.h
+++ test/Test.h
@@ -10,3 +10,9 @@ if(!(COND)) \
     std::cerr << __FILE__ << ":" << __LINE__ << " assertion (" << #COND << ") failed" << std::endl; \
     std::exit(-1); \
 }
+
+#define TEST_ASSERT_EQ(LHS, RHS) \
+if((LHS) != (RHS)) \
+{ \
+     throw AssertionException(__FILE__ ":"  #__LINE__ " : assertion failed: " #LHS "==" #RHS);
+}
