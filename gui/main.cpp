6ffbb7803450f9f667fb8619d408c7f59e09743a
derselbst <tom.mbrt@gmail.com>
2017-01-12 20:33:12 +0100

    catch excpetions

--- gui/main.cpp
+++ gui/main.cpp
@@ -13,10 +13,34 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    int ret;
+    int ret=-1;
     try
     {
         ret = a.exec();
+    }
+    catch(const logic_error& e)
+    {
+        w.hide();
+
+        CLOG(LogLevel::FATAL, "Terminated after throwing logic_error: " << e.what());
+
+        QMessageBox msgBox;
+        msgBox.setText("You've discovered a bug in some logic implementation. Please let us know the details below.");
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.setDetailedText(e.what());
+        msgBox.exec();
+    }
+    catch(const runtime_error& e)
+    {
+        w.hide();
+
+        CLOG(LogLevel::FATAL, "Terminated after throwing runtime_error: " << e.what());
+
+        QMessageBox msgBox;
+        msgBox.setText("An unhandled runtime error occurred, probably a bug. Please let us know the details below.");
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.setDetailedText(e.what());
+        msgBox.exec();
     }
     catch(const exception& e)
     {
@@ -25,12 +49,21 @@ int main(int argc, char *argv[])
         CLOG(LogLevel::FATAL, "Terminated after throwing this: " << e.what());
 
         QMessageBox msgBox;
-        msgBox.setText("The Playback unexpectedly stopped.");
+        msgBox.setText("Some kind of exception occurred, likely a bug. Please let us know the details below.");
         msgBox.setIcon(QMessageBox::Critical);
         msgBox.setDetailedText(e.what());
         msgBox.exec();
+    }
+    catch(...) // for proper stack unwinding
+    {
+        w.hide();
 
-        ret = -1;
+        CLOG(LogLevel::FATAL, "Terminated after throwing ???");
+
+        QMessageBox msgBox;
+        msgBox.setText("An unknown error occurred. If you can reproduce this, please let us know.");
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.exec();
     }
 
     return ret;
