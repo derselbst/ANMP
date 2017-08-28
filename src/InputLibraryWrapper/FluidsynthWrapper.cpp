4fd3c2d9f5ac80ed6d3b2af70ab148da5ca7ed6d
derselbst <tom.mbrt@googlemail.com>
2017-08-28 10:48:40 +0200

    attempt to fix/hack DK64's broken midi track loops

--- src/InputLibraryWrapper/FluidsynthWrapper.cpp
+++ src/InputLibraryWrapper/FluidsynthWrapper.cpp
@@ -391,13 +391,25 @@ void FluidsynthWrapper::AddEvent(smf_event_t * event, double offset)
 // in case MidiWrapper experiences a loop event, schedule an event that calls MidiWrapper back at the end of that loop, so it can feed all the midi events with that loop back to fluidsynth again
 void FluidsynthWrapper::ScheduleLoop(MidiLoopInfo* loopInfo)
 {
+    int ret;
     unsigned int callbackdate = fluid_sequencer_get_tick(this->sequencer); // now
     
     callbackdate += static_cast<unsigned int>((loopInfo->stop.Value - loopInfo->start.Value) * 1000); // postpone the callback date by the duration of this midi track loop
     
+    // HACK: before scheduling the callback, make sure no note is sounding anymore at loopend
+    // for some ambiance tunes of DK64 the noteoff events happen after the loopend, as a consequence, note the have been turned on during a note will never be stopped.
+    // this hack however breaks JFG, so disable it for now
+#if 0
+    fluid_event_all_notes_off(this->synthEvent, loopInfo->channel);
+    ret = fluid_sequencer_send_at(this->sequencer, this->synthEvent, callbackdate-1, true);
+    if(ret != FLUID_OK)
+    {
+        CLOG(LogLevel_t::Error, "fluidsynth was unable to queue midi event");
+    }
+#endif
+    
     fluid_event_timer(this->callbackEvent, loopInfo);
-
-    int ret = fluid_sequencer_send_at(this->sequencer, this->callbackEvent, callbackdate, true);
+    ret = fluid_sequencer_send_at(this->sequencer, this->callbackEvent, callbackdate, true);
     if(ret != FLUID_OK)
     {
         CLOG(LogLevel_t::Error, "fluidsynth was unable to queue midi event");
