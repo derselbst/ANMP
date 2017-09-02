eec06413560c91547e08f2bc02a1ad69eee35238
derselbst <tom.mbrt@googlemail.com>
2017-09-02 19:14:04 +0200

    add missing files

---
+++ src/Common/SampleFormat.c
@@ -0,0 +1,13 @@
+
+#include "SampleFormat.h"
+
+
+const char* SampleFormatName[] =
+{
+    [uint8] = "uint8",
+    [int16] = "int16",
+    [int32] = "int32",
+    [float32] = "float32",
+    [float64] = "float64",
+    [END] = "Srsly?",
+};
---
+++ src/Common/SampleFormat.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+enum
+#ifdef __cplusplus
+class // c99 doesnt support scoped enums, fallback to unscoped ones
+#endif
+SampleFormat_t
+{
+    BEGIN = 0,
+    unknown = BEGIN,
+    uint8,
+    int16,
+    int32,
+    float32,
+    float64,
+    END,
+};
+
+extern const char* SampleFormatName[];
+
+#ifdef __cplusplus
+}
+#endif
+
