5d568a59bf52dc5dc224aeb81cbff7b7c2ebb40c
derselbst <tom.mbrt@googlemail.com>
2018-12-15 10:39:56 +0100

    make labels selectable in song inspector

--- gui/songinspector.cpp
+++ gui/songinspector.cpp
@@ -5,6 +5,15 @@
 #include <QLabel>
 #include <anmp.hpp>
 
+class MySelectableLabel : public QLabel
+{
+public:
+    MySelectableLabel(const QString& text, QWidget *parent=nullptr) : QLabel(text, parent)
+    {
+        this->setTextInteractionFlags(Qt::TextSelectableByMouse);
+    }
+};
+
 SongInspector::SongInspector(const Song *s, QWidget *parent)
 : QDialog(parent),
   ui(new Ui::SongInspector)
@@ -26,14 +35,14 @@ void SongInspector::fillGeneral(const Song *s)
     delete oldLayout;
 
     QFormLayout *form = new QFormLayout(scroll);
-    form->addRow("File:", new QLabel(QString::fromStdString(s->Filename), scroll));
-    form->addRow("File Offset:", new QLabel(QString::number(s->fileOffset.Value), scroll));
-    form->addRow("File Length:", new QLabel(QString::number(::framesToMs(s->getFrames(), s->Format.SampleRate) / 1000.0) + " s", scroll));
-    form->addRow("Sample Rate:", new QLabel(QString::number(s->Format.SampleRate) + " Hz", scroll));
-    form->addRow("Sample Format:", new QLabel(SampleFormatName[static_cast<int>(s->Format.SampleFormat)], scroll));
-    form->addRow("Audio Channels:", new QLabel(QString::number(s->Format.Channels()), scroll));
-    form->addRow("Audio Voices:", new QLabel(QString::number(s->Format.Voices), scroll));
-    form->addRow("Bit Rate:", new QLabel(QString::number(s->Format.getBitrate() / 1024) + " kBit/s", scroll));
+    form->addRow("File:", new MySelectableLabel(QString::fromStdString(s->Filename), scroll));
+    form->addRow("File Offset:", new MySelectableLabel(QString::number(s->fileOffset.Value), scroll));
+    form->addRow("File Length:", new MySelectableLabel(QString::number(::framesToMs(s->getFrames(), s->Format.SampleRate) / 1000.0) + " s", scroll));
+    form->addRow("Sample Rate:", new MySelectableLabel(QString::number(s->Format.SampleRate) + " Hz", scroll));
+    form->addRow("Sample Format:", new MySelectableLabel(SampleFormatName[static_cast<int>(s->Format.SampleFormat)], scroll));
+    form->addRow("Audio Channels:", new MySelectableLabel(QString::number(s->Format.Channels()), scroll));
+    form->addRow("Audio Voices:", new MySelectableLabel(QString::number(s->Format.Voices), scroll));
+    form->addRow("Bit Rate:", new MySelectableLabel(QString::number(s->Format.getBitrate() / 1024) + " kBit/s", scroll));
 
     scroll->setLayout(form);
 }
@@ -45,14 +54,14 @@ void SongInspector::fillMetadata(const SongInfo &m)
     delete oldLayout;
 
     QFormLayout *form = new QFormLayout(scroll);
-    form->addRow("Composer", new QLabel(QString::fromStdString(m.Composer), scroll));
-    form->addRow("Artist", new QLabel(QString::fromStdString(m.Artist), scroll));
-    form->addRow("Album", new QLabel(QString::fromStdString(m.Album), scroll));
-    form->addRow("Title", new QLabel(QString::fromStdString(m.Title), scroll));
-    form->addRow("Track", new QLabel(QString::fromStdString(m.Track), scroll));
-    form->addRow("Year", new QLabel(QString::fromStdString(m.Year), scroll));
-    form->addRow("Genre", new QLabel(QString::fromStdString(m.Genre), scroll));
-    form->addRow("Comment", new QLabel(QString::fromStdString(m.Comment), scroll));
+    form->addRow("Composer", new MySelectableLabel(QString::fromStdString(m.Composer), scroll));
+    form->addRow("Artist", new MySelectableLabel(QString::fromStdString(m.Artist), scroll));
+    form->addRow("Album", new MySelectableLabel(QString::fromStdString(m.Album), scroll));
+    form->addRow("Title", new MySelectableLabel(QString::fromStdString(m.Title), scroll));
+    form->addRow("Track", new MySelectableLabel(QString::fromStdString(m.Track), scroll));
+    form->addRow("Year", new MySelectableLabel(QString::fromStdString(m.Year), scroll));
+    form->addRow("Genre", new MySelectableLabel(QString::fromStdString(m.Genre), scroll));
+    form->addRow("Comment", new MySelectableLabel(QString::fromStdString(m.Comment), scroll));
 
     scroll->setLayout(form);
 }
