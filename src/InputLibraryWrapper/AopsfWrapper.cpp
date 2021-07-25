derselbst/ANMP

commit 287edd2173b1af865ed5bf7bda89afa0b302c743

add separate method for audio normalization
dont do it within STANDARDWRAPPER_RENDER()

src/InputLibraryWrapper/AopsfWrapper.cpp
@@ -190,6 +190,8 @@ void AopsfWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
                                }
                            }
                           )
+    
+    this->doAudioNormalization(static_cast<int16_t*>(bufferToFill), framesToRender);
 }
 
 frame_t AopsfWrapper::getFrames () const

src/InputLibraryWrapper/LibSNDWrapper.cpp
@@ -97,38 +97,25 @@ void LibSNDWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
     constexpr bool haveInt64 = sizeof(int)==8;
 
     static_assert(haveInt32 || haveInt64, "sizeof(int) is neither 4 nor 8 bits on your platform");
-
-#define SND_AMPLIFY(TYPE, XXX) \
-    /* audio normalization factor */\
-    const float absoluteGain = (numeric_limits<TYPE>::max()) / (numeric_limits<TYPE>::max() * this->gainCorrection);\
-    for(unsigned int i=0; gConfig.useAudioNormalization && i<framesToDoNow*Channels; i++)\
-    {\
-        pcm[i].XXX = static_cast<TYPE>(pcm[i].XXX * absoluteGain);\
-    }
     
     const SampleFormat_t cachedFormat = this->Format.SampleFormat;
     
     if(haveInt32)
     {
         // no extra work necessary here, as usual write directly to pcm buffer
-        STANDARDWRAPPER_RENDER_WITH_POST_PROC(
+        STANDARDWRAPPER_RENDER(
                                 sndfile_sample_t,
                                 if(cachedFormat == SampleFormat_t::float32)
                                 {
                                     // the file contains floats, thus read floats
                                     sf_read_float(this->sndfile, reinterpret_cast<float*>(pcm), framesToDoNow*Channels);
-                                    
-                                    SND_AMPLIFY(float, f);
                                 }
                                 else if(cachedFormat == SampleFormat_t::int32)
                                 {
                                     // or read int32s
                                     sf_read_int(this->sndfile, reinterpret_cast<int32_t*>(pcm), framesToDoNow*Channels);
-                                    
-                                    SND_AMPLIFY(int32_t, i);
                                 }
                                 else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");
-                                , ;
         )
     }
     else if(haveInt64)
@@ -144,41 +131,38 @@ void LibSNDWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
             int64_temp_buf = tmp.data();
         }
         
-        STANDARDWRAPPER_RENDER_WITH_POST_PROC(
+        STANDARDWRAPPER_RENDER(
                                 sndfile_sample_t, // bufferToFill shall be of this type
                                 if(cachedFormat == SampleFormat_t::float32)
                                 {
                                     // just read out floats
                                     sf_read_float(this->sndfile, reinterpret_cast<float*>(pcm), framesToDoNow*Channels);
-                                    
-                                    SND_AMPLIFY(float, f);
                                 }
                                 else if(cachedFormat == SampleFormat_t::int32)
                                 {
                                     // hm, int is 64 bits wide. write it to temp buffer and force it to int32 and amplify it within the same run.
                                     sf_read_int(this->sndfile, int64_temp_buf, framesToDoNow*Channels);
                                     
-                                    /* audio normalization factor */
-                                    const float absoluteGain = (numeric_limits<int32_t>::max()) / (numeric_limits<int32_t>::max() * this->gainCorrection);
                                     for(unsigned int i=0; i<framesToDoNow*Channels; i++)
                                     {
                                         // see http://www.mega-nerd.com/libsndfile/api.html#note1:
                                         // Whenever integer data is moved from one sized container to another sized container, the
                                         // most significant bit in the source container will become the most significant bit in the destination container.
                                         pcm[i].i = static_cast<int32_t>(int64_temp_buf[i] >> ((sizeof(int) - sizeof(sndfile_sample_t))*8)/*==32*/ );
-                                        
-                                        if(gConfig.useAudioNormalization)
-                                        {
-                                            pcm[i].i = static_cast<int32_t>(pcm[i].i * absoluteGain); // so sry for that nesting...
-                                        }
                                     }
                                 }
                                 else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");
-                                , ; // postprocession already done :/
         )
     }
     
-#undef SND_AMPLIFY
+    if(cachedFormat == SampleFormat_t::float32)
+    {
+        this->doAudioNormalization(static_cast<float*>(bufferToFill), framesToRender);
+    }
+    else if(cachedFormat == SampleFormat_t::int32)
+    {
+        this->doAudioNormalization(static_cast<int32_t*>(bufferToFill), framesToRender);
+    }
 }
 
 vector<loop_t> LibSNDWrapper::getLoopArray () const noexcept

src/InputLibraryWrapper/MidiWrapper.cpp
@@ -367,5 +367,7 @@ vector<loop_t> MidiWrapper::getLoopArray () const noexcept
 void MidiWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
 {
     STANDARDWRAPPER_RENDER(float, this->synth->Render(pcm, framesToDoNow))
+    
+    this->doAudioNormalization(static_cast<float*>(bufferToFill), framesToRender);
 }
 #undef FramesToRender

src/InputLibraryWrapper/ModPlugWrapper.cpp
@@ -142,6 +142,8 @@ void ModPlugWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
                            int ret = ModPlug_Read(this->handle, pcm, framesToDoNow*Channels*sizeof(int32_t));
                            framesToDoNow = ret / (Channels * sizeof(int32_t));
                           )
+    
+    this->doAudioNormalization(static_cast<int32_t*>(bufferToFill), framesToRender);
 }
 
 frame_t ModPlugWrapper::getFrames () const

src/InputLibraryWrapper/StandardWrapper.h
@@ -32,15 +32,17 @@
   * @warning whenever changing this implementation, dont forget LibMadWrapper::render() AND FFMpegWrapper::render(), which does
   * pretty much the same thing, without using this macro
   */
-#define STANDARDWRAPPER_RENDER_WITH_POST_PROC(SAMPLEFORMAT, LIB_SPECIFIC_RENDER_FUNCTION, POST_PROCESSING) \
+#define STANDARDWRAPPER_RENDER(SAMPLEFORMAT, LIB_SPECIFIC_RENDER_FUNCTION) \
+{\
+    decltype(framesToRender) backup;\
     if(framesToRender==0)\
     {\
         /* render rest of file */ \
-        framesToRender = this->getFrames()-this->framesAlreadyRendered;\
+        backup = framesToRender = this->getFrames()-this->framesAlreadyRendered;\
     }\
     else\
     {\
-        framesToRender = min(framesToRender, this->getFrames()-this->framesAlreadyRendered);\
+        backup = framesToRender = min(framesToRender, this->getFrames()-this->framesAlreadyRendered);\
     }\
     const uint32_t Channels = this->Format.Channels(); \
     SAMPLEFORMAT* pcm = static_cast<SAMPLEFORMAT*>(bufferToFill);\
@@ -55,25 +57,15 @@
         /* call the function whatever is responsible for decoding to raw pcm */\
         LIB_SPECIFIC_RENDER_FUNCTION;\
 \
-        /* actually do the audio normalization or anything else here */\
-        POST_PROCESSING;\
-\
         /* advance the pcm pointer by that many items that have just been rendered */\
         pcm += (framesToDoNow * Channels) % this->count;\
         this->framesAlreadyRendered += framesToDoNow;\
 \
         framesToRender -= framesToDoNow;\
     }\
+    framesToRender = backup;\
+}
 
-#define DO_AUDIO_NORMALIZATION(SAMPLEFORMAT) \
-    /* audio normalization factor */\
-    const float absoluteGain = (numeric_limits<SAMPLEFORMAT>::max()) / (numeric_limits<SAMPLEFORMAT>::max() * this->gainCorrection);\
-    for(unsigned int i=0; gConfig.useAudioNormalization && i<framesToDoNow*Channels; i++)\
-    {\
-        pcm[i] = static_cast<SAMPLEFORMAT>(pcm[i] * absoluteGain);\
-    }
-    
-#define STANDARDWRAPPER_RENDER(SAMPLEFORMAT, LIB_SPECIFIC_RENDER_FUNCTION) STANDARDWRAPPER_RENDER_WITH_POST_PROC(SAMPLEFORMAT, LIB_SPECIFIC_RENDER_FUNCTION, DO_AUDIO_NORMALIZATION(SAMPLEFORMAT))
 
 template<typename SAMPLEFORMAT>
 class StandardWrapper : public Song
@@ -106,6 +98,9 @@ protected:
 
     template<typename WRAPPERCLASS>
     void fillBuffer(WRAPPERCLASS* context);
+    
+    template<typename REAL_SAMPLEFORMAT>
+    void doAudioNormalization(REAL_SAMPLEFORMAT* bufferToFill, const frame_t framesToProcess);
 
     // used for sound normalization
     // usual range: (0,1]

src/InputLibraryWrapper/StandardWrapper_impl.h
@@ -139,4 +139,30 @@ frame_t StandardWrapper<SAMPLEFORMAT>::getFramesRendered()
 {
     return this->framesAlreadyRendered;
 }
+
+template<typename IGNORE>
+template<typename SAMPLEFORMAT>
+void StandardWrapper<IGNORE>::doAudioNormalization(SAMPLEFORMAT* bufferToFill, const frame_t framesToProcess)
+{
+    if(!gConfig.useAudioNormalization)
+    {
+        return;
+    }
+    
+    const uint32_t Channels = this->Format.Channels();
+    SAMPLEFORMAT* pcm = static_cast<SAMPLEFORMAT*>(bufferToFill);
+    
+    /* advance the pcm pointer by that many items where we previously ended filling it */
+    pcm += (this->getFramesRendered() * Channels) % this->count;
+    
+    const unsigned int itemsToProcess = framesToProcess*Channels;
+    /* audio normalization factor */
+    const float AbsoluteGain = (numeric_limits<SAMPLEFORMAT>::max()) / (numeric_limits<SAMPLEFORMAT>::max() * this->gainCorrection);
+    #pragma omp parallel for default(none) firstprivate(pcm) shared(itemsToProcess, AbsoluteGain)
+    for(unsigned int i=0; i<itemsToProcess && !this->stopFillBuffer; i++)
+    {
+        pcm[i] = static_cast<SAMPLEFORMAT>(pcm[i] * AbsoluteGain);
+    }
+}
+
 #endif

src/InputLibraryWrapper/VGMStreamWrapper.cpp
@@ -83,6 +83,8 @@ void VGMStreamWrapper::fillBuffer()
 void VGMStreamWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
 {
     STANDARDWRAPPER_RENDER(int16_t, render_vgmstream(pcm, framesToDoNow, this->handle))
+    
+    this->doAudioNormalization(static_cast<int16_t*>(bufferToFill), framesToRender);
 }
 
 vector<loop_t> VGMStreamWrapper::getLoopArray () const noexcept

src/InputLibraryWrapper/FFMpegWrapper.cpp
@@ -299,6 +299,9 @@ void FFMpegWrapper::render(pcm_t* bufferToFill, frame_t framesToRender)
 
 
     av_frame_free(&frame);
+    
+    
+    this->doAudioNormalization(static_cast<int16_t*>(bufferToFill), framesToRender);
 }
 
 frame_t FFMpegWrapper::getFrames () const

commit 9f45b56772936eb1a0c24851fcc71b9398fcab59

Fix audio normalization
It was broken in 287edd2173b1af865ed5bf7bda89afa0b302c743.

src/InputLibraryWrapper/AopsfWrapper.cpp
@@ -179,8 +179,6 @@ void AopsfWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, fr
                                    THROW_RUNTIME_ERROR("PSF emulation failed with unknown error.");
                                }
                            })
-
-    this->doAudioNormalization(static_cast<int16_t *>(bufferToFill), framesToRender);
 }
 
 frame_t AopsfWrapper::getFrames() const

src/InputLibraryWrapper/LazyusfWrapper.cpp
@@ -99,9 +99,10 @@ void LazyusfWrapper::close() noexcept
 
 void LazyusfWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, frame_t framesToRender)
 {
-// TODO: UGLY CAST AHEAD!
+    int32_t rate = this->Format.SampleRate;
 STANDARDWRAPPER_RENDER(int16_t,
-                       usf_render(this->usfHandle, pcm, framesToDoNow, reinterpret_cast<int32_t *>(&this->Format.SampleRate)))
+                       usf_render(this->usfHandle, pcm, framesToDoNow, &rate))
+
 }
 
 frame_t LazyusfWrapper::getFrames() const

src/InputLibraryWrapper/LibSNDWrapper.cpp
@@ -136,15 +136,17 @@ void LibSNDWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, f
     if (haveInt32)
     {
         // no extra work necessary here, as usual write directly to pcm buffer
-        STANDARDWRAPPER_RENDER(
-        sndfile_sample_t,
         if (cachedFormat == SampleFormat_t::float32) {
+            STANDARDWRAPPER_RENDER(
+            float,
             // the file contains floats, thus read floats
-            sf_read_float(this->sndfile, reinterpret_cast<float *>(pcm), framesToDoNow * Channels);
+            sf_read_float(this->sndfile, reinterpret_cast<float *>(pcm), framesToDoNow * Channels))
         } else if (cachedFormat == SampleFormat_t::int32) {
+            STANDARDWRAPPER_RENDER(
+            int32_t,
             // or read int32s
-            sf_read_int(this->sndfile, reinterpret_cast<int32_t *>(pcm), framesToDoNow * Channels);
-        } else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");)
+            sf_read_int(this->sndfile, reinterpret_cast<int32_t *>(pcm), framesToDoNow * Channels))
+        } else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");
     }
     else if (haveInt64)
     {
@@ -159,12 +161,14 @@ void LibSNDWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, f
             int64_temp_buf = tmp.data();
         }
 
-        STANDARDWRAPPER_RENDER(
-        sndfile_sample_t, // bufferToFill shall be of this type
         if (cachedFormat == SampleFormat_t::float32) {
+            STANDARDWRAPPER_RENDER(
+            float,
             // just read out floats
-            sf_read_float(this->sndfile, reinterpret_cast<float *>(pcm), framesToDoNow * Channels);
+            sf_read_float(this->sndfile, reinterpret_cast<float *>(pcm), framesToDoNow * Channels))
         } else if (cachedFormat == SampleFormat_t::int32) {
+            STANDARDWRAPPER_RENDER(
+            int32_t,
             // hm, int is 64 bits wide. write it to temp buffer and force it to int32 and amplify it within the same run.
             sf_read_int(this->sndfile, int64_temp_buf, framesToDoNow * Channels);
 
@@ -173,18 +177,10 @@ void LibSNDWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, f
                 // see http://www.mega-nerd.com/libsndfile/api.html#note1:
                 // Whenever integer data is moved from one sized container to another sized container, the
                 // most significant bit in the source container will become the most significant bit in the destination container.
-                pcm[i].i = static_cast<int32_t>(int64_temp_buf[i] >> ((sizeof(int) - sizeof(sndfile_sample_t)) * 8) /*==32*/);
+                pcm[i] = static_cast<int32_t>(int64_temp_buf[i] >> ((sizeof(int) - sizeof(sndfile_sample_t)) * 8) /*==32*/);
             }
-        } else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");)
-    }
-
-    if (cachedFormat == SampleFormat_t::float32)
-    {
-        this->doAudioNormalization(static_cast<float *>(bufferToFill), framesToRender);
-    }
-    else if (cachedFormat == SampleFormat_t::int32)
-    {
-        this->doAudioNormalization(static_cast<int32_t *>(bufferToFill), framesToRender);
+            )
+        } else THROW_RUNTIME_ERROR("THIS SHOULD NEVER HAPPEN: SampleFormat neither int32 nor float32");
     }
 }
 

src/InputLibraryWrapper/MidiWrapper.cpp
@@ -400,6 +400,4 @@ vector<loop_t> MidiWrapper::getLoopArray() const noexcept
 void MidiWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, frame_t framesToRender)
 {
     STANDARDWRAPPER_RENDER(float, this->synth->Render(pcm, framesToDoNow))
-
-    this->doAudioNormalization(static_cast<float *>(bufferToFill), framesToRender);
 }

src/InputLibraryWrapper/ModPlugWrapper.cpp
@@ -139,8 +139,6 @@ void ModPlugWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, 
     STANDARDWRAPPER_RENDER(int32_t,
                            int ret = ModPlug_Read(this->handle, pcm, framesToDoNow * Channels * sizeof(int32_t));
                            framesToDoNow = ret / (Channels * sizeof(int32_t));)
-
-    this->doAudioNormalization(static_cast<int32_t *>(bufferToFill), framesToRender);
 }
 
 frame_t ModPlugWrapper::getFrames() const

src/InputLibraryWrapper/N64CSeqWrapper.cpp
@@ -886,6 +886,4 @@ vector<loop_t> N64CSeqWrapper::getLoopArray() const noexcept
 void N64CSeqWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, frame_t framesToRender)
 {
     STANDARDWRAPPER_RENDER(float, this->synth->Render(pcm, framesToDoNow))
-
-    this->doAudioNormalization(static_cast<float *>(bufferToFill), framesToRender);
 }

src/InputLibraryWrapper/OpenMPTWrapper.cpp
@@ -74,8 +74,6 @@ void OpenMPTWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, 
                            {
                                break;
                            })
-
-    this->doAudioNormalization(static_cast<float *>(bufferToFill), framesToRender);
 }
 
 frame_t OpenMPTWrapper::getFrames() const

src/InputLibraryWrapper/StandardWrapper.h
@@ -44,6 +44,7 @@
                                                                                                                                                   \
             /* call the function whatever is responsible for decoding to raw pcm */                                                               \
             LIB_SPECIFIC_RENDER_FUNCTION;                                                                                                         \
+            this->doAudioNormalization(pcm, framesToDoNow);                                                                                       \
                                                                                                                                                   \
             /* advance the pcm pointer by that many items that have just been rendered */                                                         \
             pcm += (framesToDoNow * Channels) % this->count;                                                                                      \

src/InputLibraryWrapper/StandardWrapper_impl.h
@@ -13,11 +13,11 @@ void StandardWrapper<IGNORE>::doAudioNormalization(SAMPLEFORMAT *pcm, const fram
     }
 
     const uint32_t Channels = this->Format.Channels();
-    const unsigned int itemsToProcess = framesToProcess * Channels;
+    const frame_t itemsToProcess = framesToProcess * Channels;
 
     /* audio normalization factor */
     const float AbsoluteGain = (numeric_limits<SAMPLEFORMAT>::max()) / (numeric_limits<SAMPLEFORMAT>::max() * this->gainCorrection);
-    for (unsigned int i = 0; i < itemsToProcess && !this->stopFillBuffer; i++)
+    for (frame_t i = 0; i < itemsToProcess && !this->stopFillBuffer; i++)
     {
         pcm[i] = static_cast<SAMPLEFORMAT>(pcm[i] * AbsoluteGain);
     }

src/InputLibraryWrapper/VGMStreamWrapper.cpp
@@ -69,8 +69,6 @@ void VGMStreamWrapper::close() noexcept
 void VGMStreamWrapper::render(pcm_t *const bufferToFill, const uint32_t Channels, frame_t framesToRender)
 {
     STANDARDWRAPPER_RENDER(int16_t, render_vgmstream(pcm, framesToDoNow, this->handle))
-
-    this->doAudioNormalization(static_cast<int16_t *>(bufferToFill), framesToRender);
 }
 
 vector<loop_t> VGMStreamWrapper::getLoopArray() const noexcept