554624f968fdb3ab96399bd14247700154f7bc5c
Tom M <tom.mbrt@googlemail.com>
2022-02-12 23:33:57 +0100

    Update IAudioOutput.h

--- src/AudioOutput/IAudioOutput.h
+++ src/AudioOutput/IAudioOutput.h
@@ -132,7 +132,7 @@ class IAudioOutput
      * @param outChannels number of audio channels in @p out buffer
      */
     template<typename TIN, typename TOUT = TIN>
-    void Mix(const frame_t frames, const TIN *restrict in, const SongFormat &inputFormat, TOUT *restrict out) noexcept;
+    void Mix(const frame_t frames, const TIN *RESTRICT in, const SongFormat &inputFormat, TOUT *RESTRICT out) noexcept;
 
     /**
      * pushes the pcm pointed to by buffer to the underlying audio driver and by that causes it to play
