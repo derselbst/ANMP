7521c7b5afee8a957d169a64366088329fe254ff
derselbst <tom.mbrt@googlemail.com>
2022-02-19 22:55:39 +0100

    TEST_ASSERT_EQ

--- test/TestLoudnessFile.cpp
+++ test/TestLoudnessFile.cpp
@@ -14,23 +14,23 @@ int main()
 
     float gain = 2.0f;
     LoudnessFile::write(testFile, gain);
-    TEST_ASSERT(LoudnessFile::read(testFile) == gain);
+    TEST_ASSERT_EQ(LoudnessFile::read(testFile), gain);
 
     gain = 0.0001f;
     LoudnessFile::write(testFile, gain);
-    TEST_ASSERT(LoudnessFile::read(testFile) == gain);
+    TEST_ASSERT_EQ(LoudnessFile::read(testFile), gain);
 
     gain = 0.00f;
     LoudnessFile::write(testFile, gain);
-    TEST_ASSERT(LoudnessFile::read(testFile) == gain);
+    TEST_ASSERT_EQ(LoudnessFile::read(testFile), gain);
 
     gain = -0.00f;
     LoudnessFile::write(testFile, gain);
-    TEST_ASSERT(LoudnessFile::read(testFile) == gain);
+    TEST_ASSERT_EQ(LoudnessFile::read(testFile), gain);
 
     gain = -1.9876f;
     LoudnessFile::write(testFile, gain);
-    TEST_ASSERT(LoudnessFile::read(testFile) == gain);
+    TEST_ASSERT_EQ(LoudnessFile::read(testFile), gain);
 
     return 0;
 }
